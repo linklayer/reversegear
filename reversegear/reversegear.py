21c6d791b28e186a3c0d98dad62490d52bbe4e5e
Eric Evenchick <eric@evenchick.com>
2017-08-24 15:18:57 -0400

    better output formatting

--- reversegear/reversegear.py
+++ reversegear/reversegear.py
@@ -2,13 +2,17 @@ import argparse
 
 from .uds import UDSDecoder
 
-__version__ = "0.0.1"
+__version__ = '0.0.1'
+
 
 def auto_int(x):
     return int(x, 0)
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Offline reverse engineering tools for automotive networks.')
+    parser = argparse.ArgumentParser(
+        description='Offline reverse engineering tools for automotive'
+        'networks.')
     parser.add_argument('--debug', action='store_true',
                         help='enable debug output')
 
@@ -16,9 +20,11 @@ def main():
                                        help='additional help')
     uds_parser = subparsers.add_parser('uds')
     uds_parser.add_argument('tx_id', type=auto_int,
-                             help="CAN arbitration ID transmitted by the scan tool")
+                            help='CAN arbitration ID transmitted'
+                            'by the scan tool')
     uds_parser.add_argument('rx_id', type=auto_int,
-                             help="CAN arbitration ID received by the scan tool")
+                            help='CAN arbitration ID received'
+                            'by the scan tool')
     uds_parser.add_argument('inputs', nargs='+')
     uds_parser.set_defaults(func=uds_func)
 
@@ -29,8 +35,9 @@ def main():
     else:
         parser.print_help()
 
+
 def uds_func(args):
-    decoder = UDSDecoder()
+    decoder = UDSDecoder(debug=args.debug)
     for f in args.inputs:
         session = decoder.read_file(f, args.tx_id, args.rx_id)
         decoder.generate_output(session)
--- reversegear/uds.py
+++ reversegear/uds.py
@@ -1,10 +1,15 @@
 import pprint
+import string
 from pyvit.hw.logplayer import LogPlayer
 from pyvit.dispatch import Dispatcher
 from pyvit.proto.isotp import IsotpInterface
 from pyvit.proto.uds import *
 
+
 class UDSDecoder:
+    def __init__(self, debug=False):
+        self.debug = debug
+
     def read_file(self, filename, tx_id, rx_id):
         lp = LogPlayer(filename, realtime=False)
         disp = Dispatcher(lp)
@@ -19,13 +24,21 @@ class UDSDecoder:
                 req = uds_req.decode_request()
                 if req is None:
                     break
+
+                if self.debug:
+                    print(req)
                 session.append(req)
 
                 resp = None
                 # wait until we get a response
                 # this will return None if there is a response pending
-                while resp is None:
+                tries = 0
+                while resp is None and tries < 5:
                     resp = uds_resp.decode_response()
+                    tries = tries + 1
+
+                if self.debug:
+                    print(resp)
                 session.append(resp)
 
             except NegativeResponseException as e:
@@ -39,15 +52,49 @@ class UDSDecoder:
             if isinstance(r, GenericRequest):
                 print('\n[->] Request [%s / 0x%X]' % (r.name, r.SID))
                 for k in r.keys():
-                    print('\t%s: %s' % (k, pprint.pformat(r[k], indent=8,
-                                                        compact=True)))
+                    print('%s: ' % k, end='')
+                    print(self.format_data(r[k]))
+                    print('')
+
             elif isinstance(r, GenericResponse):
                 print('[<-] Response [%s / 0x%X]' % (r.name, r.SID))
                 for k in r.keys():
-                    print('\t%s: %s' % (k, pprint.pformat(r[k], indent=8,
-                                                        compact=True)))
+                    print('%s: ' % k, end='')
+                    print(self.format_data(r[k]))
+                    print('')
 
             elif isinstance(r, NegativeResponseException):
                 print('\n[!!] %s' % r)
             elif r is None:
                 print('Unknown Service')
+
+    def format_data(self, data, line_len=8):
+        result = ''
+        if isinstance(data, int):
+            result = '0x%X' % data
+        elif isinstance(data, list):
+            result += '\n'
+            for addr in range(0, len(data), line_len):
+                # add the address
+                result += '%03X: ' % addr
+
+                # add line of data as hex
+                for b in data[addr:addr+line_len]:
+                    result += '%02X ' % b
+
+                # if this is the last line, pad it
+                if int(addr / 8) == int(len(data) / line_len):
+                    for _ in range(0, line_len - (len(data) % line_len)):
+                        result += '-- '
+
+                result += '\t'
+
+                # add line of data as ascii
+                for b in data[addr:addr+line_len]:
+                    if chr(b) in string.printable and b >= 32 and b < 0x7F:
+                        result += chr(b)
+                    else:
+                        result += '.'
+
+                result += '\n'
+        return result
